0626d08728e7c4da87efd32fc0a46425ccb9e1f7
root <root@DESKTOP-D9GDID1.localdomain>
2020-04-20 14:18:32 -0600

    Attempted at this. Added a nested for loop with an if statement. Researched the use of 'swap' and hopefully did this file correctly..

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -9,5 +9,32 @@ void swap(int *x, int *y)
 
 void bubbleSort(int *begin, const int *end)
 {
+	int k;
+	int p;
+	int h = k+1;
+	int baby = k!=end;
+	int yoda = p!=end;
+
+	for(k=begin, baby,k++) {
+	
+	 	for(p=h; yoda, p++) {
+	       		
+
+			if(*k > *(h)) {
+				
+				swap(k, h); //used sawp right here.
+			}
+		}
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//Use swap to exchange two values
 }
