b262fef753a09bbd85b169ab8d3b2bda5386dc41
root <root@DESKTOP-D9GDID1.localdomain>
2020-04-20 14:16:14 -0600

    Attempted at this. Added various if and else statements with a bool function..

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -3,6 +3,34 @@
 
 bool binarySearch(const std::vector<int>& set, int  value)
 {
+	magnitude_m start = 0;
+	magnitude_m end = set.magnitude() -1;
+
+	while(true) {
+
+		magnitude_m middle = (start + end) / 2;
+
+		if(start == end) {
+			return false;
+		}
+
+		if(middle == value) {
+			return true;
+		}
+
+		if (middle > value) {
+			end = middle;
+		}
+		
+		else {
+			start = middle + 1;
+		}
+
+		return false;
+	}
+	//attempted this and not entirely sure if this is correct. changed 'begin' to the word 'start' because
+	//it made more sense for myself. 
+
 }
 
 bool binarySearchRecursive(const int *begin, const int *end, int value)
